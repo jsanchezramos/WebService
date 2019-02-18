d62d29169400d4223f5f5817bca2fa04ee3d6a44
juanfranciscosanchezramos <F3ar0fm3>
2019-02-18 14:14:22 +0100

    clean code..

--- src/main/java/service/ServiceAuthentication.java
+++ src/main/java/service/ServiceAuthentication.java
@@ -34,22 +34,36 @@ public class ServiceAuthentication {
     private Token validateNumTokenAndTime(Token tokenObject,int limitTimeSecons,int limitTimeMinuts){
         Token tokenIncremental = null;
 
+        if(checkDifferentsTimeAndAccesTokenMinorLimitTimeSeconAndMinutsIsZero(tokenObject,limitTimeSecons)){
+            tokenIncremental = new Token(tokenObject.getToken(),tokenObject.getAccesToken(),tokenObject.getNumToken() + INCREMENTAL_TOKEN);
+        }else if (checkDifferentsTimeAndAccesTokenMayorLimitTimeSeconAndMinutsIsZero(tokenObject,limitTimeSecons)) {
+            tokenIncremental = new Token(tokenObject.getToken(),tokenObject.getAccesToken(),ONE_TOKEN);
+        }else if(checkDifferentsTimeInMinuts(tokenObject,limitTimeMinuts)) {
+            tokenIncremental = new Token(tokenObject.getToken(),TokenHelper.getCurrent(),ONE_TOKEN);
+        }
+        return tokenIncremental;
+    }
+
+    private Boolean checkDifferentsTimeAndAccesTokenMinorLimitTimeSeconAndMinutsIsZero(Token tokenObject,int limitTimeSecons){
         if(TokenHelper.checkDifferentsSecons(tokenObject.getAccesToken()) < limitTimeSecons
                 && TokenHelper.checkMaxAuth(tokenObject.getNumToken())
                 && TokenHelper.checkDifferentsMinutes(tokenObject.getAccesToken()) == CERO_MINUTS){
-
-            tokenIncremental = new Token(tokenObject.getToken(),tokenObject.getAccesToken(),tokenObject.getNumToken() + INCREMENTAL_TOKEN);
-
-        }else if (TokenHelper.checkDifferentsSecons(tokenObject.getAccesToken()) > limitTimeSecons
+            return true;
+        }else
+            return false;
+    }
+    private Boolean checkDifferentsTimeAndAccesTokenMayorLimitTimeSeconAndMinutsIsZero(Token tokenObject,int limitTimeSecons) {
+        if(TokenHelper.checkDifferentsSecons(tokenObject.getAccesToken()) > limitTimeSecons
                 && TokenHelper.checkMaxAuth(tokenObject.getNumToken())
-                && TokenHelper.checkDifferentsMinutes(tokenObject.getAccesToken()) == CERO_MINUTS) {
-
-            tokenIncremental = new Token(tokenObject.getToken(),tokenObject.getAccesToken(),ONE_TOKEN);
-
-        }else if(TokenHelper.checkDifferentsMinutes(tokenObject.getAccesToken()) >= limitTimeMinuts) {
-            tokenIncremental = new Token(tokenObject.getToken(),TokenHelper.getCurrent(),ONE_TOKEN);
-        }
-
-        return tokenIncremental;
+                && TokenHelper.checkDifferentsMinutes(tokenObject.getAccesToken()) == CERO_MINUTS){
+            return true;
+        }else
+            return false;
+    }
+    private Boolean checkDifferentsTimeInMinuts(Token tokenObject,int limitTimeMinuts){
+        if(TokenHelper.checkDifferentsMinutes(tokenObject.getAccesToken()) >= limitTimeMinuts){
+            return true;
+        }else
+            return false;
     }
 }
